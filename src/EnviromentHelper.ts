88e1948569b9146e9c698bb8976f829e6419943e
mast76 <martin@stenderup.dk>
2024-06-28 13:24:35 +0200

    Update EnviromentHelper.ts

--- src/EnviromentHelper.ts
+++ src/EnviromentHelper.ts
@@ -56,78 +56,48 @@ export enum GithubContext {
     inputs
 }
 
-export function parseKey(key) {
-    key = key.trim()
-    const mInt = /^-?(\d+|(0x[0-9A-F]+))$/gi
-    const mFlt = /^-?((\d*.\d+)(e-?\d+)?)$/gi
-    const mStr1 = /^'(.+)'$/gi
-    const mStr2 = /^"(.+)"$/gi
-    
-    if(key?.match(mStr1)) {
-      key = key.replace(mStr1,'$1')
-      key = key.replaceAll("''","'")
-    } if(key?.match(mStr2)) {
-      key = key.replace(mStr2,'$1')
-      key = key.replaceAll('""','"')
-    } else if(key?.match(mInt)) {
-        key = Number.parseInt(key)
-    } else if(key?.match(mFlt)) {
-        key = Number.parseFloat(key)
-    } else if(key == 'true') {
-        key = true
-    } else if(key == 'false') {
-        key = false
-    } else if(key == 'null') {
-        key = null
-    }
+export function replaceEnvVariables(key, localEnv : GitHubEnv) {
+            
+    let ctx = key.substring(0,key.indexOf('.'))
+    //console.log(ctx)
 
-    return key
+    key = key.substring(key.indexOf('.')+1)
+
+    switch (ctx) {
+        case 'env':
+            val = localEnv ? localEnv[key] : ''
+            break
+        case 'github':
+            val = localEnv ? localEnv['GITHUB_' + key] : ''
+            break
+        case 'runner':
+            val = localEnv ? localEnv['RUNNER_' + key] : ''
+            break
+        default:
+            val = "''"
+    }
+    return val
+}
+
+export function parseKey(exp : GitHubEnv) {
+    exp = exp.trim()
+    const mStr1 = /'([^'])'/gi
+    const env = /[a-z]+\.[a-z]+/gi
+    
+    exp = exp.replaceAll("''","'")
+    
+    exp?.match(mStr1)?.forEach(m => {
+        exp = exp.replace(m,'$1')
+    })
+    exp?.match(env)?.forEach(m =>  {
+        exp = exp.replace(m, replaceEnvVariables(m, localEnv))
+    })
+
+    return exp
 }
 
 export function replaceExpression(str, localEnv : GitHubEnv) {
-    str = parseKey(str)
-
-    const m = /\${{([^}]+)}}/gs;
-    const c = /^\w+\.\w+/g
-    
-    let s;
-    str?.match(m)?.forEach(s => {
-        //console.log('s:' + s)
-        let key = s.replace(m, '$1');
-        key = parseKey(key)
-        //console.log('key:' + key)
-        var val = '';
-        
-        if(key.match(c)) {
-            
-            let ctx = key.substring(0,key.indexOf('.'))
-            ctx = GithubContext[ctx]
-            //console.log(ctx)
-
-            key = key.substring(key.indexOf('.')+1)
-
-            switch (ctx) {
-                case GithubContext.env:
-                    val = localEnv ? localEnv[key] : ''
-                    break
-                case GithubContext.github:
-                    val = localEnv ? localEnv['GITHUB_' + key] : ''
-                    break
-                case GithubContext.runner:
-                    val = localEnv ? localEnv['RUNNER_' + key] : ''
-                    break
-                default:
-                    val = 'Not Implemented!'
-            }
-        }
-        
-        //console.log('val:' + val)
-        
-        if (val) {
-            str = str.replace(s, val);
-        }
-    });
-    //console.log(str)
+    str = parseKey(str, localEnv)
     return str;
 }
 
